d2e943caf11368f98587fb3d974b1b864fcce41c
Agustín Méndez <matagus@gmail.com>
2012-10-28 17:34:32 -0300

    Some typos fixed.

--- generic_links/forms.py
+++ generic_links/forms.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 from django import forms
 
-from generic_link.models import GenericLink
+from generic_links.models import GenericLink
 
 
 class AddLinkForm(forms.ModelForm):
@@ -13,14 +13,14 @@ class AddLinkForm(forms.ModelForm):
         self.user = user
         self.content_type = content_type
         self.object_id = object_id
-        super(AddForm, self).__init__(*args, **kwargs)
+        super(AddLinkForm, self).__init__(*args, **kwargs)
 
     def save(self, *args, **kwargs):
-        self.instance, created = GenericLink.objects.create(
+        self.instance = GenericLink.objects.create(
             content_type=self.content_type, object_id=self.object_id,
             url=self.cleaned_data["url"].strip(),
             title=self.cleaned_data["title"], user=self.user,
-            is_approved=self.cleaned_data["is_external"])
+            is_external=self.cleaned_data["is_external"])
 
         return self.instance
 
