5b3d86f78f0fa76ae1b98ac0033b884cdbef12d7
Agustín Méndez <matagus@gmail.com>
2012-10-28 17:33:47 -0300

    Typo fixed and content type issue fixed.

--- generic_links/views.py
+++ generic_links/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
+from django.db.models import get_model
 from django.http import Http404
 from django.shortcuts import render_to_response, HttpResponseRedirect
 from django.template import RequestContext
+from django.contrib.contenttypes.models import ContentType
 
 from generic_links.forms import AddLinkForm
 
@@ -16,14 +18,16 @@ def add_link(request, app_model, object_id):
     except Model.DoesNotExist:
         raise Http404
 
+    content_type = ContentType.objects.get(app_label=app, model=model)
+
     if request.method == "POST":
-        form = AddForm(request.user, content_type, object_id, data=request.POST)
+        form = AddLinkForm(request.user, content_type, object_id, data=request.POST)
         if form.is_valid():
             link = form.save()
             return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
     else:
-        form = AddForm(request.user, content_type, object_id)
+        form = AddLinkForm(request.user, content_type, object_id)
 
-    return render_to_response("generic_links/links/add.html",
-        {"object": object, "app_model": app_model, "form": form},
+    return render_to_response("generic_links/add.html",
+        {"object": an_object, "app_model": app_model, "form": form},
         context_instance=RequestContext(request))
