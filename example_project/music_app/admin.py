3d58841992bf12fc14e4b7d3093ddbc1950d2351
Agustín Méndez <matagus@gmail.com>
2024-02-06 20:49:10 -0600

    Add example project with an app using generic_links

---
+++ example_project/music_app/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+
+from generic_links.admin import GenericLinkStackedInline
+
+from music_app.models import Album, Artist
+
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ["title", "artist", "release_date", "created_at", "updated_at"]
+    list_filter = ["artist"]
+    search_fields = ["title", "artist__name"]
+    list_per_page = 10
+    inlines = [GenericLinkStackedInline]
+
+
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ["name", "created_at", "updated_at"]
+    search_fields = ["name"]
+    list_per_page = 10
+    inlines = [GenericLinkStackedInline]
