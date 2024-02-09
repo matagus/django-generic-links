02e0c61e5fbbd610fe2d2a1c1aacc8146cd43933
Agustín Méndez <matagus@gmail.com>
2024-02-09 04:14:36 -0600

    Adding one missing test for models.py to take coverage to 100%

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,21 +1,23 @@
-"""
-test_generic_links
-------------
-
-Tests for `generic_links` models module.
-"""
-
 from __future__ import annotations
 
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
 from django.test import TestCase
 
+from generic_links.models import GenericLink
 
-class TestGeneric_links(TestCase):
-    def setUp(self):
-        pass
 
-    def test_something(self):
-        pass
+class ModelTest(TestCase):
+    def test_str(self):
+        user = User.objects.create(username="Test User")
+        content_type = ContentType.objects.get_for_model(User)
 
-    def tearDown(self):
-        pass
+        link = GenericLink.objects.create(
+            title="Test Title",
+            url="https://www.test.com",
+            description="Test Description",
+            content_type=content_type,
+            object_id=user.id,
+        )
+
+        self.assertEqual(str(link), f"{link.title} :: {link.url}")
