03df0406a0f8ff7d6ad0faede9456ae6ce329472
Agustín Méndez <matagus@gmail.com>
2024-02-10 11:24:54 -0600

    Updated README with usage examples for reverse generic relation

--- example_project/music_app/models.py
+++ example_project/music_app/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from django.contrib.contenttypes.fields import GenericRelation
 
 
 class Artist(models.Model):
     name = models.CharField(max_length=100)
     bio = models.TextField()
+
+    # This is important so that we can get the GenericLink related instances for an object of this model
+    # See https://docs.djangoproject.com/en/5.0/ref/contrib/contenttypes/#reverse-generic-relations
+    generic_links = GenericRelation("generic_links.GenericLink")
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
